b05bd63df595a45bb1cd5bc2b3a70343a7aa49d0
valorad <wcxaaa@gmail.com>
2017-02-04 13:51:44 +0800

    * further anim

--- src/app/animation/animation.component.ts
+++ src/app/animation/animation.component.ts
@@ -22,8 +22,15 @@ import {
       state('on', style({
         backgroundColor: 'white'
       })),
-      transition('off => on', [animate('1s')]),
-      transition('on => off', [animate('1s')])
+      // transition('off => on', [animate('1s')]),
+      // transition('on => off', [animate('1s')])
+
+      // style仅在当前animate中生效，不会在外面的另外的state中生效。见下面的例子。把角度改为180deg。
+      //transition('off <=> on', [animate('2s', style({ transform: 'rotate(360deg)' }))])
+
+      // ease-in: slow->fast, ease-out: fast->slow. full list available in easings.net
+      transition('off => on', [animate('2000ms 2s ease-in', style({ transform: 'rotate(360deg)' }))]),
+      transition('on => off', [animate('2000ms ease-out', style({ transform: 'rotate(-360deg)' }))])
     ])
   ]
 })
@@ -32,7 +39,12 @@ export class AnimationComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
   }
+
+  promptMsg = {
+    shown: false
+  };
 
   public cdkRoomLightsStatus = {
     "on": "bright",
@@ -43,6 +55,25 @@ export class AnimationComponent implements OnInit {
 
   toggleLight() {
     this.cdkRoomLightCurrent = (this.cdkRoomLightCurrent === "off")? "on": "off";
+  }
+
+  report(e) {
+    console.log(`Anim from state: ${ e.fromState }`);
+    console.log(`Anim to state: ${ e.toState }`);
+    console.log(`Anim time elapsed: ${ e.totalTime }`);
+  }
+
+  animStart(e) {
+    var msg = this.promptMsg;
+    msg.shown = true;
+    setTimeout("10", function(){msg.shown = false})
+    console.log("Animation started");
+    this.report(e);
+  }
+
+  animEnd(e) {
+    this.report(e);
+    console.log("Animation ended");
   }
 
 }
